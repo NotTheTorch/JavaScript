0e8df7a3399739455b4fedfc96ac9be75138277a
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-04-24 16:47:55 +0530

    Save Data and generate HTML for Payment summary

--- AmazonClone/scripts/checkout/orderSummary.js
+++ AmazonClone/scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import {cart,updateQuantity,updateCartQuantity_checkout,removeFromCart,updateDeliveryOption} from '../../data/cart.js'
-import { products } from '../../data/products.js';
+import { getProduct, products } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://cdn.jsdelivr.net/npm/dayjs@2.0.0-alpha.2/dist/index.mjs';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 
 export function renderOrderSummary()
 {
@@ -11,22 +11,12 @@ export function renderOrderSummary()
     cart.forEach((cartItem)=>{
 
         const productId = cartItem.id;
-        let matchingProduct;
+        const matchingProduct = getProduct(productId);
 
-        products.forEach((product)=>{
-
-            if(product.id === productId)
-                matchingProduct = product;
-
-        });
 
         const deliveryOptionId = cartItem.deliveryOptionId;
-        let deliveryOption;
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
-        deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId)
-                deliveryOption = option;
-        });
 
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays,'days');
--- AmazonClone/scripts/utils/money.js
+++ AmazonClone/scripts/utils/money.js
@@ -3,6 +3,6 @@ import {products} from '../../data/products.js'
 
 
 export function formatCurrency(priceCents){
-    return ((priceCents/100).toFixed(2));
+    return ((Math.round(priceCents)/100).toFixed(2));
 }
 
