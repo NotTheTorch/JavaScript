5717046c21770954f6b6d9ab8a86efb2f0164170
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-04-28 06:54:51 +0530

    Create automated tests to add to cart

--- AmazonClone/data/cart.js
+++ AmazonClone/data/cart.js
@@ -1,17 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [
-    {
-        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-        quantity: 2,
-        deliveryOptionId: '1'
-    },
+export let cart;
 
-    {
-        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-        quantity: 1,
-        deliveryOptionId: '2'
-    }
+loadFromStorage();
 
-];
+export function loadFromStorage()
+{
+    cart = JSON.parse(localStorage.getItem('cart')) || [
+        {
+            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+            quantity: 2,
+            deliveryOptionId: '1'
+        },
+    
+        {
+            id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            quantity: 1,
+            deliveryOptionId: '2'
+        }
+    ];
+}
 
 function saveToStorage()
 {
