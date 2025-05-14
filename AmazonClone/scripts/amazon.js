0bee01c369adbdcba6c5659b665dde0292bd708e
NotTheTorch <vishnutejashanmukha928@gmail.com>
2025-05-14 13:06:47 +0530

    Load Product form the backend

--- AmazonClone/scripts/amazon.js
+++ AmazonClone/scripts/amazon.js
@@ -1,82 +1,88 @@
 import {addToCart, updateCartQuantity_checkout} from "../data/cart.js";
-import { products } from "../data/products.js"; 
+import { products,loadProducts } from "../data/products.js"; 
 
-let productsHTML = '';
+loadProducts(renderProductsGrid);
 
 
-products.forEach((product)=>{
-    productsHTML += `<div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="${product.image}">
-          </div>
+function renderProductsGrid()
+{
+    let productsHTML = '';
 
-          <div class="product-name limit-text-to-2-lines">
-            ${product.name}
-          </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="${product.getStarsURL()}">
-            <div class="product-rating-count link-primary">
-              ${product.rating.count}
+    products.forEach((product)=>{
+        productsHTML += `<div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image"
+                src="${product.image}">
             </div>
-          </div>
 
-          <div class="product-price">
-            ${product.getPrice()}
-          </div>
+            <div class="product-name limit-text-to-2-lines">
+                ${product.name}
+            </div>
 
+            <div class="product-rating-container">
+                <img class="product-rating-stars"
+                src="${product.getStarsURL()}">
+                <div class="product-rating-count link-primary">
+                ${product.rating.count}
+                </div>
+            </div>
 
-          <div class="product-quantity-container">
-            <select class="js-quantity-selector-${product.id}">
-              <option selected value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          ${product.extraInfoHTML()}
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart js-added-to-cart-${product.id}">
-            <img src="images/icons/checkmark.png">
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-            Add to Cart
-          </button>
-        </div>`;
-
-});
+            <div class="product-price">
+                ${product.getPrice()}
+            </div>
 
 
-document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity_checkout();
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+            <div class="product-quantity-container">
+                <select class="js-quantity-selector-${product.id}">
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                </select>
+            </div>
 
-const fadeTimers = {};
+            ${product.extraInfoHTML()}
 
-document.querySelectorAll('.js-add-to-cart')
-.forEach((button)=>{
-    button.addEventListener('click',()=>{
-        const productID = button.dataset.productId;
-        const added = document.querySelector(`.js-added-to-cart-${productID}`);
-        const quantity = Number(document.querySelector(`.js-quantity-selector-${productID}`).value);
-        
-        added.classList.add('added-opacity');
-        clearTimeout(fadeTimers[productID]);
-        fadeTimers[productID] = setTimeout(()=> added.classList.remove('added-opacity'),2000);
+            <div class="product-spacer"></div>
 
-        addToCart(productID,quantity);
-        document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity_checkout();
+            <div class="added-to-cart js-added-to-cart-${product.id}">
+                <img src="images/icons/checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+                Add to Cart
+            </button>
+            </div>`;
+
     });
-});
+
+
+    document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity_checkout();
+    document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+    const fadeTimers = {};
+
+    document.querySelectorAll('.js-add-to-cart')
+    .forEach((button)=>{
+        button.addEventListener('click',()=>{
+            const productID = button.dataset.productId;
+            const added = document.querySelector(`.js-added-to-cart-${productID}`);
+            const quantity = Number(document.querySelector(`.js-quantity-selector-${productID}`).value);
+            
+            added.classList.add('added-opacity');
+            clearTimeout(fadeTimers[productID]);
+            fadeTimers[productID] = setTimeout(()=> added.classList.remove('added-opacity'),2000);
+
+            addToCart(productID,quantity);
+            document.querySelector('.js-cart-quantity').innerHTML = updateCartQuantity_checkout();
+        });
+    });
+}
--- AmazonClone/scripts/checkout/orderSummary.js
+++ AmazonClone/scripts/checkout/orderSummary.js
@@ -158,5 +158,3 @@ export function renderOrderSummary()
         });
     });
 }
-
-renderOrderSummary();
